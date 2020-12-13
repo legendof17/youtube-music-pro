dfe3190bd678acce80ac37557a21b9551bfb6a48
Jaffer Ali <jafferali150@gmail.com>
2020-12-14 05:06:42 +0530

    Created Home Page

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Badge} from 'react-bootstrap'
+
+const Home = () => {
+    return (
+        <div>
+            <h1><Badge pill variant="warning">Hello World<Badge pill variant="success">~YMP</Badge></Badge></h1>
+        </div>
+    )
+}
+
+export default Home
