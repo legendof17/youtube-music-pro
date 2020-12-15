2e75946f9a03610893c78b7dbb6c076fedf6a813
Jaffer Ali <jafferali150@gmail.com>
2020-12-15 05:30:53 +0530

    Added FullScreen Button on the Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
-import { Navbar1, NavbarBrand } from './navbarelements';
+import { Navbar1, NavbarBrand, toggleFullScreen } from './navbarelements';
 
 const Navi = () => {
     return (
@@ -13,6 +13,7 @@ const Navi = () => {
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="link">Link</Nav.Link>
                     <Nav.Link href="github">GitHub</Nav.Link>
+                    <Nav.Link onClick={toggleFullScreen}>F</Nav.Link>
                     </Nav>
                 </Navbar1.Collapse>
             </Navbar1>
