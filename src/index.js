3f13b714b47bd1171f158a512d5a25a383895827
Jaffer Ali <jafferali150@gmail.com>
2020-12-16 03:58:09 +0530

    more SEO optimization

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-jinke-music-player/assets/index.css'
 
 ReactDOM.render(
