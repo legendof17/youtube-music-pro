863f294ab5ab005d265f38a5e1354ef724403fca
Jaffer Ali <jafferali150@gmail.com>
2020-12-15 05:32:39 +0530

    Fixed Body container scrollbar on default & FullS

--- src/pages/pageelements.js
+++ src/pages/pageelements.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export const Body = styled.div`
-    min-height: 100vh;
+    min-height: 90vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     font-size: calc(10px + 2vmin);
     color: white;
+    font-family: 'Nerko One';
 `;
