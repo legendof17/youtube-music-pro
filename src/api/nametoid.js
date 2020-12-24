57b06d42b97ab0f214400ca09997dde4e2aa94d4
Jaffer Ali <jafferali150@gmail.com>
2020-12-25 03:31:35 +0530

    turned off console log of id

--- src/api/nametoid.js
+++ src/api/nametoid.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default async function Nametoid(name) {
     let id = await axios.get('http://localhost:6001/http://youtube-scrape.herokuapp.com/api/search?q='+name)
-    console.log(id)
+    // console.log(id)
     return id['data']['results'][0]['video']['id']
 }
