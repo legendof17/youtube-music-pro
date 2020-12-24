6a2e81642bb34da6fe0862798e030af932797c4d
Jaffer Ali <jafferali150@gmail.com>
2020-12-25 03:35:46 +0530

    remastered ultimate centralised api controller

--- src/api/ultimateapi.js
+++ src/api/ultimateapi.js
@@ -1,33 +1,53 @@
 import React from 'react'
 import Assetfinder from './assetfinder';
+import Infoextract from './infoextract';
 import Nametoid from './nametoid';
+import Songstatusfixer from './songstatusfixer';
 
 export default class Ultimateapi extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            status: 0,
+            id: null,
             songname: null,
             artist: null,
             thumbnail: null,
             songurl: null,
-            state: 0,
-            id: null
         }
         this.query_name = props.name;
     }
 
+    async initrun(name) {
+        let id = await Nametoid(name)
+        let data = await Infoextract(id)
+        data['url'] = await Songstatusfixer(data['url'],id)
+        this.setState({status: await Assetfinder(data['url'])})
+        return data
+    }
+
     async componentDidMount() {
-        this.setState({id : await Nametoid(this.query_name)})
-        this.setState({status : await Assetfinder(this.query_name)})
+        const data = await this.initrun(this.query_name)
+        this.setState({
+            songname: data['title'],
+            artist: data['artist'],
+            thumbnail: data['thumbnail'],
+            songurl: data['url'],
+            id: data['id'],
+        })
+        // console.log(data)
     }
 
     render() {
         return (
             <div>
-                <h1>{this.query_name}</h1>
-                <h1>status: {this.state.status}</h1>
-                <h1>id: {this.state.id}</h1>
-                <h1>hello</h1>
+                <h1>Hello</h1>
+                <h1>Status: {this.state.status}</h1>
+                <h1>ID: {this.state.id}</h1>
+                <h1>SongName: {this.state.songname}</h1>
+                <h1>Artist: {this.state.artist}</h1>
+                <h1>Thumbnail: {this.state.thumbnail}</h1>
+                <h1>Songurl: {this.state.songurl}</h1>
             </div>
         )
     }
