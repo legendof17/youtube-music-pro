31bef556e83b9a7272b4fd072e294ebed74050ba
Jaffer Ali <jafferali150@gmail.com>
2020-12-28 18:19:17 +0530

    Linked api with native server with CORS support

--- src/api/proapi.js
+++ src/api/proapi.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export default async function ProApi(songName,dn) {
-    const ownedproxy = 'https://ntop-cors.herokuapp.com/'
-    let response = await axios.get(ownedproxy+'https://youtube-music-pro.herokuapp.com/proapi?name='+songName+'&dn='+dn)
+    // const ownedproxy = 'https://ntop-cors.herokuapp.com/'
+    // let response = await axios.get(ownedproxy+'https://youtube-music-pro.herokuapp.com/proapi?name='+songName+'&dn='+dn)
+    const apiurl = 'https://youtube-music-pro.herokuapp.com/proapi?name='
+    let response = await axios.get(apiurl+songName+'&dn='+dn)
     // console.log(response['data'])
     return response['data']
 }
