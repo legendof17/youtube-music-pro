3d2290e0232ac5a2d1f152e95ccdeddc5805afa2
Jaffer Ali <jafferali150@gmail.com>
2020-12-29 23:48:07 +0530

    improved neon intro

--- src/components/Home/NeonIntro/index.js
+++ src/components/Home/NeonIntro/index.js
@@ -3,7 +3,9 @@ import './style.css'
 const Neon = () => {
     return (
         <div className="sign">
-            <span className="fast-flicker">Y</span>outube<span className="flicker"><br />Music</span>Pro
+            <span className="fast-flicker">Y</span>o<span className="fast-flicker">ut</span>ub<span className="fast-flicker">e</span><span className="flicker">
+                <br />
+                <span className="fast-flicker">M</span>us<span className="fast-flicker">ic</span></span>P<span className="fast-flicker">ro</span>
         </div>
     )
 }
