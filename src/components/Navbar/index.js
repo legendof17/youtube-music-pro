8db224ab73a443aa0d99d6d89fc48d69647c26b4
Jaffer Ali <jafferali150@gmail.com>
2020-12-29 21:16:38 +0530

    removed unused components

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
 import { Navbar1, NavbarBrand, toggleFullScreen } from './navbarelements';
 
 const Navi = () => {
