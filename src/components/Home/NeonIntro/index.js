7589895f6c194eed0e17017288cde823c2fa985a
Jaffer Ali <jafferali150@gmail.com>
2020-12-29 21:13:51 +0530

    Added new neon intro component

---
+++ src/components/Home/NeonIntro/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const Neon = () => {
+    return (
+        <div className="sign">
+            <span className="fast-flicker">Y</span>outube<span className="flicker"><br />Music</span>Pro
+        </div>
+    )
+}
+export default Neon
