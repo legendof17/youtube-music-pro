a66c3702411056212e3bcff20f0caf25d2cb23b2
Jaffer Ali <jafferali150@gmail.com>
2020-12-31 03:01:40 +0530

    added reference url link to profile and project

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import {Nav} from 'react-bootstrap';
-import { Navbar1, NavbarBrand, toggleFullScreen } from './navbarelements';
+import { Navbar1, NavbarBrand, toggleFullScreen, Copyright } from './navbarelements';
 
 const Navi = () => {
     return (
@@ -13,10 +13,13 @@ const Navi = () => {
                 <Navbar1.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="link">Link</Nav.Link>
-                    <Nav.Link as={Link} to="github">GitHub</Nav.Link>
-                    <Nav.Link onClick={toggleFullScreen}>F</Nav.Link>
+                    {/* <Nav.Link as={Link} to="Legendof17">Legendof17</Nav.Link> */}
+                    <Nav.Link href={"https://github.com/legendof17"}>Legendof17</Nav.Link>
+                    {/* <Nav.Link as={Link} to="github">GitHub</Nav.Link> */}
+                    <Nav.Link href={"https://github.com/legendof17/youtube-music-pro"}>GitHub</Nav.Link>
+                    {/* <Nav.Link onClick={toggleFullScreen}>© JASM 2020</Nav.Link> */}
                     </Nav>
+                    <Copyright onClick={toggleFullScreen}>© JASM 2020</Copyright>
                 </Navbar1.Collapse>
             </Navbar1>
         </div>
