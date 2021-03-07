351d93c3b4d762af8edbda439843a2b58b1ba135
Jaffer Ali <jafferali150@gmail.com>
2021-03-08 00:39:24 +0530

    Added Legendof17 Neon

---
+++ src/components/Home/NeonIntro/legendof17.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+const Legendof17 = () => {
+    return (
+        <div className="sign">
+            <span className="fast-flicker">L</span>eg<span className="fast-flicker">e</span>nd
+            <br />
+            <span className="fast-flicker">o</span><span className="flicker">f
+            <span className="fast-flicker">1</span>7
+            <br />
+            <span className="fast-flicker">♡</span></span>
+        </div>
+    )
+}
+export default Legendof17
