317bece934a950eddae154ff91b56f49008ec2ee
Jaffer Ali <jafferali150@gmail.com>
2021-03-08 00:39:09 +0530

    Added TopBeast Neon

---
+++ src/components/Home/NeonIntro/topbeast.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const TopBeast = () => {
+    return (
+        <div className="sign">
+            <span className="fast-flicker">T</span>o<span className="fast-flicker">p</span>{' '}Be<span className="fast-flicker">a</span><span className="flicker">st
+                <br />
+                <span className="fast-flicker">2</span>0<span className="fast-flicker">2</span></span>1
+        </div>
+    )
+}
+export default TopBeast
